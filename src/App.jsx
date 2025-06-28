223c363b7c12678906580f2631427b9ef1d48302
Deepak-900 <deepakmaharjan900@gmail.com>
2025-06-28 16:12:28 +0545

    Added Logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
-import './App.css'
-
+import './App.css';
+import logo from './assets/logo.png'
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-3xl   font-bold underline">
         Hello world!
+        <img src={logo} className='w-[75px]' alt="" />
       </h1>
     </>
   )
